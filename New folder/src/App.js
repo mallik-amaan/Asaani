21fb130c37d1e49545b452bfb38440a5c0ef1b93
Malik Muhammad Aman <maman.bscs22seecs@seecs.edu.pk>
2024-04-30 15:20:31 +0500

    paths added

--- New folder/src/App.js
+++ New folder/src/App.js
@@ -1,25 +1,29 @@
-import React from 'react';
-import Navbar from './navbar'; 
-import './App.css'; 
-import Footer from './footer'; 
-
-
+import React from "react";
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
+import "./App.css";
 function App() {
   return (
-    <><><><div className="app">
-      <Navbar />
-      <Footer />
-    </div><div className="centered-text">
-        <h1>ہر مشکل کام اب ہوگا آسان</h1>
-      </div></><div className="centered-text2">
-        <h1>آسانی</h1>
-      </div></><div className="auth-buttons">
+    <>
+      <>
+        <>
+          <div className="app">
+            <Navbar />
+            <Footer />
+          </div>
+          <div className="centered-text">
+            <h1>ہر مشکل کام اب ہوگا آسان</h1>
+          </div>
+        </>
+        <div className="centered-text2">
+          <h1>آسانی</h1>
+        </div>
+      </>
+      <div className="auth-buttons">
         <button className="login-btn">Login</button>
         <button className="signup-btn">Signup</button>
-      </div></>
-    
-    
-  
+      </div>
+    </>
   );
 }
 
--- New folder/src/Components/Navbar.js
+++ New folder/src/Components/Navbar.js
@@ -1,18 +1,26 @@
-import React from 'react';
-import './App.css'; 
+import React from "react";
+import "../App.css";
 
 const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="navbar-heading">Asaani</div>
       <ul className="navbar-nav">
-        <li className="nav-item"><a href="#">Home</a></li>
-        <li className="nav-item"><a href="#">About</a></li>
-        <li className="nav-item"><a href="#">Services</a></li>
-        <li className="nav-item"><a href="#">Contact</a></li>
+        <li className="nav-item">
+          <a href="#">Home</a>
+        </li>
+        <li className="nav-item">
+          <a href="#">About</a>
+        </li>
+        <li className="nav-item">
+          <a href="#">Services</a>
+        </li>
+        <li className="nav-item">
+          <a href="#">Contact</a>
+        </li>
       </ul>
     </nav>
   );
-}
+};
 
 export default Navbar;
