18e5278512c755f662a38d777029f94832033f6c
Kaif Ul Imaan <113798422+kaifulimaan@users.noreply.github.com>
2024-05-01 22:29:57 +0500

    db connection setup with azure mysql

--- Back-end/server.js
+++ Back-end/server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
+const mysql = require('mysql');
+
+
+
+
 
 app.get('/', (req, res) => {
     res.send('Hello from Asaani server');
 });
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
