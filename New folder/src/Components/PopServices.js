298da54699bbba49929a79df1b8b30a1cd9de584
Ammar-bin-Akram <makram.bscs22seecs@seecs.edu.pk>
2024-05-11 17:07:34 +0500

    Complete design of dashboard.js    .

--- New folder/src/Components/PopServices.js
+++ New folder/src/Components/PopServices.js
@@ -2,20 +2,17 @@ import React from 'react'
 import GigCard from './GigCard'
 function PopServices() {
   return (
-    <div>
-        <div className="d-flex align-items-center justify-content-between">
-          <h3 className="my-4 mx-4">Popular Services</h3>
-          <a href="/" style={{"text-decoration": "none", color: "black"}}><h5 className="mx-5">See all</h5></a>
-        </div>
       <div style={{padding:"2%"}}>
-        <h3>Popular Services</h3>
+        <div className="d-flex align-items-center justify-content-between">
+            <h3 className="my-4 mx-4">Popular Services</h3>
+            <a href="/" style={{"text-decoration": "none", color: "black"}}><h5 className="mx-5">See all</h5></a>
+        </div>
         <div className="row"style={{justifyContent:"space-between",margin:"2%"}}>
           <GigCard className="col" />
           <GigCard className="col" />
           <GigCard className="col" />
         </div>
       </div>
-    </div>
   );
 }
 
