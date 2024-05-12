d8f2b478896b0fd7a6f5f4905a9e247932d20eb3
Ahadhassan-2003 <ahadhassankhakwani@gmail.com>
2024-05-12 16:54:34 +0500

    payments&billings screen made

--- New folder/src/Components/Buttons.js
+++ New folder/src/Components/Buttons.js
@@ -1,8 +1,8 @@
 import * as React from "react";
 import Button from "@mui/material/Button";
 
-function Buttons({value,varient,btsize}) {
-  return <Button variant={varient} size={btsize}>{value}</Button>;
+function Buttons({value,varient,btsize,onClick}) {
+  return <Button variant={varient} size={btsize} onClick={onClick}>{value}</Button>;
 }
 
 export default Buttons
--- New folder/src/Screens/Payment&Billing.js
+++ New folder/src/Screens/Payment&Billing.js
@@ -1,20 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import ColoredTab from "../Components/ColoredTab";
 import CreditCardForm from "../Components/CreditCardForm";
 import CreditCardIcon from '@mui/icons-material/CreditCard';
+import Button from "../Components/Buttons";
+import CreditCard from "../Components/PaymentCard";
+
 
 function PaymentBilling() {
   return (
     <div className="PaymentBilling">
-        
-            <ColoredTab/>
-            <div className="row">
-                <CreditCardIcon className="col-lg"/>
-                <p>Credit Card</p>
-                <CreditCardForm/>
+      <div className="container">
+        <div className="row">
+          <div className="col-6">
+            <div style={{display:'flex', padding:'20px 0px 0px 10px'}}>
+              <CreditCardIcon/>
+              <h5 style={{paddingLeft:'10px'}}>Credit Card(s)</h5>
             </div>
+            <div style={{paddingLeft:'25px'}}>
+              <p style={{paddingTop:'5px'}}>Manage your credit cards and payment options</p>
+            </div>
+            <div style={{padding:'20px'}}>
+              <CreditCardForm/>
+            </div>
+          </div>
+          <div className="col-6" style={{padding:'20px'}}>
+            <CreditCard paddingtopprop="20px" cardtype='visa'/>
+            <CreditCard paddingtopprop="20px" cardtype='master'/>
+            <CreditCard paddingtopprop="20px" cardtype='visa'/>
+            <CreditCard paddingtopprop="20px" cardtype='paypal'/>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
+
 
 export default PaymentBilling;
