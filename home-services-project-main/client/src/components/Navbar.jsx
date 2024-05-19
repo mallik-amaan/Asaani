e6e9b5c4aa795160bb19e48aa52dd30d41cd5b49
Kaif Ul Imaan <113798422+kaifulimaan@users.noreply.github.com>
2024-05-19 12:53:30 +0500

    .
    .

--- home-services-project-main/client/src/components/Navbar.jsx
+++ home-services-project-main/client/src/components/Navbar.jsx
@@ -1,40 +1,37 @@
-import HouseIcon from "../assets/HouseIcon.svg";
-import { useNavigate } from "react-router-dom";
-import { useAuth } from "../contexts/authentication";
-import person from "../assets/homepagePhoto/person.svg";
-import form from "../assets/homepagePhoto/form.svg";
-import watch from "../assets/homepagePhoto/watch.svg";
-import exit from "../assets/homepagePhoto/exit.svg";
-import chatRoundDots from "../assets/homepagePhoto/chat-round-dots.svg.svg"; // Import the new chat icon
-import { useState } from "react";
-import PropTypes from 'prop-types';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../contexts/authentication';
+import person from '../assets/homepagePhoto/person.svg';
+import form from '../assets/homepagePhoto/form.svg';
+import watch from '../assets/homepagePhoto/watch.svg';
+import exit from '../assets/homepagePhoto/exit.svg';
+import chatRoundDots from '../assets/homepagePhoto/chat-round-dots.svg.svg';
 
-function Navbar(props) {
+function Navbar() {
   const [open, setOpen] = useState(false);
-  const loginRole = localStorage.getItem("role");
-  const loginName = localStorage.getItem("fullName");
+  const loginRole = localStorage.getItem('role');
+  const loginName = localStorage.getItem('fullName');
   const { logout } = useAuth();
   const auth = useAuth();
   const navigate = useNavigate();
 
-  const userId = localStorage.getItem("user_id");
+  const userId = localStorage.getItem('user_id');
 
   return (
     <div className="flex justify-between p-5 shadow-[2px_2px_24px_rgba(23,51,106,0.12)] sticky top-0 z-[100] bg-white">
       <div className="flex lg:ml-40 items-center">
-        <img src={HouseIcon} alt="" />
         <button
           className="text-2xl font-semibold text-[#336DF2] prompt ml-2"
-          onClick={() => navigate("/")}
+          onClick={() => navigate('/')}
         >
           Asaani
         </button>
         <div className="items-center text-base font-bold prompt lg:ml-10 px-3 pt-1.5">
           <button
             className="cursor-pointer"
-            onClick={() => navigate("/services-list")}
+            onClick={() => navigate('/services-list')}
           >
-            {props.title}
+            Our services
           </button>
         </div>
       </div>
@@ -43,140 +40,71 @@ function Navbar(props) {
         {auth.isAuthenticated ? (
           <>
             <p className="mr-4 text-[#646C80]">{loginName}</p>
-            {loginRole === "admin" ? (
-              <div className="relative">
-                <div className="flex items-center justify-center w-[30px] h-[30px] rounded-full border border-grey600 overflow-hidden cursor-pointer">
-                  <img
-                    src={person}
-                    className="w-[25px] h-[25px] rounded-full"
-                    onClick={() => setOpen(!open)}
-                  />
-                </div>
-                {open && (
-                  <div className="absolute right-0 mt-2 bg-white w-[200px] rounded-lg">
-                    <ul className="flex flex-col shadow-2xl self-stretch rounded-lg text-[#4B5160] overflow-hidden">
-                      <button
-                        className="flex p-2 hover:bg-[#EFEFF2] hover:text-[#232630]"
-                        onClick={() => navigate("/profile")}
-                      >
-                        <span>
-                          <img src={person} className="mr-3 " />
-                        </span>
-                        User information
-                      </button>
-                      <button
-                        className="flex p-1 hover:bg-[#EFEFF2] hover:text-[#232630]"
-                        onClick={() =>
-                          navigate(`/customer-ordered-list/${userId}`)
-                        }
-                      >
-                        <span>
-                          <img src={form} className="mr-3" />
-                        </span>
-                        Repair orders
-                      </button>
-                      <button
-                        className="flex pt-2 p-1 hover:bg-[#EFEFF2] hover:text-[#232630]"
-                        onClick={() =>
-                          navigate(`/customer-ordered-history/${userId}`)
-                        }
-                      >
-                        <span>
-                          <img src={watch} className="mr-3" />
-                        </span>
-                        Repair history
-                      </button>
-                      <button
-                        className="flex pt-2 p-1 hover:bg-[#EFEFF2] hover:text-[#232630]"
-                        onClick={() => navigate("/admin-category")}
-                      >
-                        <span>
-                          <img src={watch} className="mr-3" />
-                        </span>
-                        Admin Dashboard
-                      </button>
-                      <button
-                        className="flex pt-2 p-2 border-t border-grey300 hover:bg-[#EFEFF2] hover:text-[#232630]"
-                        onClick={logout}
-                      >
-                        <span>
-                          <img src={exit} className="mr-3" />
-                        </span>
-                        Log out
-                      </button>
-                    </ul>
-                  </div>
-                )}
+            <div className="relative">
+              <div className="flex items-center justify-center w-[30px] h-[30px] rounded-full border border-grey600 overflow-hidden cursor-pointer">
+                <img
+                  src={person}
+                  className="w-[25px] h-[25px] rounded-full"
+                  onClick={() => setOpen(!open)}
+                />
               </div>
-            ) : (
-              <div className="relative">
-                <div className="flex items-center justify-center w-[30px] h-[30px] rounded-full border border-grey600 overflow-hidden cursor-pointer">
-                  <img
-                    src={person}
-                    className="w-[25px] h-[25px] rounded-full"
-                    onClick={() => setOpen(!open)}
-                  />
+              {open && (
+                <div className="absolute right-0 mt-2 bg-white w-[200px] rounded-lg">
+                  <ul className="flex flex-col shadow-2xl self-stretch rounded-lg text-[#4B5160] overflow-hidden">
+                    <button
+                      className="flex p-2 hover:bg-[#EFEFF2] hover:text-[#232630]"
+                      onClick={() => navigate('/profile')}
+                    >
+                      <span>
+                        <img src={person} className="mr-3 " />
+                      </span>
+                      User information
+                    </button>
+                    <button
+                      className="flex p-1 hover:bg-[#EFEFF2] hover:text-[#232630]"
+                      onClick={() =>
+                        navigate(`/customer-ordered-list/${userId}`)
+                      }
+                    >
+                      <span>
+                        <img src={form} className="mr-3" />
+                      </span>
+                      Repair orders
+                    </button>
+                    <button
+                      className="flex pt-2 p-1 hover:bg-[#EFEFF2] hover:text-[#232630]"
+                      onClick={() =>
+                        navigate(`/customer-ordered-history/${userId}`)
+                      }
+                    >
+                      <span>
+                        <img src={watch} className="mr-3" />
+                      </span>
+                      Repair history
+                    </button>
+                    <button
+                      className="flex pt-2 p-2 border-t border-grey300 hover:bg-[#EFEFF2] hover:text-[#232630]"
+                      onClick={logout}
+                    >
+                      <span>
+                        <img src={exit} className="mr-3" />
+                      </span>
+                      Log out
+                    </button>
+                  </ul>
                 </div>
-                {open && (
-                  <div className="absolute right-0 mt-2 bg-white w-[200px] rounded-lg">
-                    <ul className="flex flex-col shadow-2xl self-stretch rounded-lg text-[#4B5160] overflow-hidden">
-                      <button
-                        className="flex p-2 hover:bg-[#EFEFF2] hover:text-[#232630]"
-                        onClick={() => navigate("/profile")}
-                      >
-                        <span>
-                          <img src={person} className="mr-3 " />
-                        </span>
-                        User information
-                      </button>
-                      <button
-                        className="flex p-1 hover:bg-[#EFEFF2] hover:text-[#232630]"
-                        onClick={() =>
-                          navigate(`/customer-ordered-list/${userId}`)
-                        }
-                      >
-                        <span>
-                          <img src={form} className="mr-3" />
-                        </span>
-                        Repair orders
-                      </button>
-                      <button
-                        className="flex pt-2 p-1 hover:bg-[#EFEFF2] hover:text-[#232630]"
-                        onClick={() =>
-                          navigate(`/customer-ordered-history/${userId}`)
-                        }
-                      >
-                        <span>
-                          <img src={watch} className="mr-3" />
-                        </span>
-                        Repair history
-                      </button>
-                      <button
-                        className="flex pt-2 p-2 border-t border-grey300 hover:bg-[#EFEFF2] hover:text-[#232630]"
-                        onClick={logout}
-                      >
-                        <span>
-                          <img src={exit} className="mr-3" />
-                        </span>
-                        Log out
-                      </button>
-                    </ul>
-                  </div>
-                )}
-              </div>
-            )}
-            <div
-              className="flex items-center justify-center w-[30px] h-[30px] ml-3 rounded-full border-grey600 cursor-pointer"
-              onClick={() => navigate("/chat")} // Make the icon clickable
-            >
+              )}
+            </div>
+            <div className="flex items-center justify-center w-[30px] h-[30px] ml-3 rounded-full border-grey600 cursor-pointer">
               <img
-                src={chatRoundDots} // Replace bell with chat-round-dots
+                src={chatRoundDots}
                 className="w-[30px] h-[30px] rounded-full bg-grey100"
+                onClick={() => navigate('/chat')}
               />
             </div>
           </>
         ) : (
-          <button className="btn-secondary" onClick={() => navigate("/login")}>
+          <button className="btn-secondary" onClick={() => navigate('/login')}>
             Log in
           </button>
         )}
@@ -186,8 +114,3 @@ function Navbar(props) {
 }
 
 export default Navbar;
-
-
-Navbar.propTypes = {
-  title: PropTypes.string.isRequired,
-};
