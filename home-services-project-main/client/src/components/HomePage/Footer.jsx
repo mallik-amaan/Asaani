98960106479eb34b7410fcd9afebc76ed067a03a
Malik Muhammad Aman <maman.bscs22seecs@seecs.edu.pk>
2024-05-19 20:24:34 +0500

    .

--- home-services-project-main/client/src/components/HomePage/Footer.jsx
+++ home-services-project-main/client/src/components/HomePage/Footer.jsx
@@ -7,30 +7,37 @@ function Footer () {
   return (
     <footer>
       <div className="flex py-2 lg:py-5 xl:p-12 xl:px-40 justify-between">
-      <div className="flex items-center">
-      <img src={HouseIcon} alt="" className="w-[40px] hidden xl:block"/>
-        <div className="lg:text-[30px] font-semibold text-[#336DF2] prompt md:ml-2">
-          Asaani
+        <div className="flex items-center">
+          <img src={HouseIcon} alt="" className="w-[40px] hidden xl:block" />
+          <div className="lg:text-[30px] font-semibold text-[#336DF2] prompt md:ml-2">
+            Asaani
           </div>
         </div>
-      <div className="">
-        <h1 className="lg:text-[18px] font-bold">Asaani</h1>
-        <p className="lg:pt-3 text-[10px] md:text-[16px]">Islamabad Sector H-12</p>
-      </div>
-      <div className="">
-        <div className="flex ">
-          <img src={Phone} alt="" className="w-[12px] lg:w-[20px]"/>
-
-          <p className="text-[12px] lg:text-[18px] xl:ml-3">+92 313444880</p>
-
+        <div className="">
+          <h1 className="lg:text-[18px] font-bold">Asaani</h1>
+          <p className="lg:pt-3 text-[10px] md:text-[16px]">
+            Islamabad Sector H-12
+          </p>
         </div>
-        <div className="flex items-center pt-3">
-          <img src={LetterIcon} alt="" className="w-[12px] lg:w-[20px] hidden sm:block"/>
-          <p className="xl:ml-3 text-[12px] lg:text-[18px]">asaani@gmail.com</p>
+        <div className="">
+          <div className="flex ">
+            <img src={Phone} alt="" className="w-[12px] lg:w-[20px]" />
+
+            <p className="text-[12px] lg:text-[18px] xl:ml-3">+92 313444880</p>
+          </div>
+          <div className="flex items-center pt-3">
+            <img
+              src={LetterIcon}
+              alt=""
+              className="w-[12px] lg:w-[20px] hidden sm:block"
+            />
+            <p className="xl:ml-3 text-[12px] lg:text-[18px]">
+              asaani@gmail.com
+            </p>
+          </div>
         </div>
       </div>
-      </div>
-      <div className="bg-[#EFEFF2] flex justify-between p-2 md:px-40">
+      <div className="bg-[#1d232a] flex justify-between p-2 md:px-40">
         <div className="text-[12px] text-grey500">
           copyright © 2024 Asaani All rights reserved
         </div>
@@ -40,7 +47,7 @@ function Footer () {
         </div>
       </div>
     </footer>
-  )
+  );
 }
 
 export default Footer;
